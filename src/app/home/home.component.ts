fea79b5746a09f9d41a2e2c1de0f18262df272e2
unknown <sukeshpsk2@gmail.com>
2018-07-17 18:46:00 -0500

    dashboard changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,10 +12,18 @@ export class HomeComponent implements OnInit {
 
   homeData: any = {};
 
-  restaurantOpenClose: boolean;
-  delivery: boolean;
+  restaurantOpenClose: boolean; // is restaurant opne or not
+  dineIn: boolean; // is dine in open or not
 
-  chartData: any;
+  chartData: any; // data for chart
+
+  deliveryOption : Array<string> = ["Pickup","Delivery", "Delivery/Pickup"];      // delivery options in the dropdown
+  selectedDeliveryOption: string = "";                                            // to display selected option
+
+  type = "line";
+
+  fontColor: any;
+
   
 
   constructor(private dashboardService: DashboardService) { 
@@ -24,7 +32,17 @@ export class HomeComponent implements OnInit {
           "caption": "Harry's SuperMart",
           "numberprefix": "$",
           "theme": "fint",
-          "numDivLines": "0"
+          "numDivLines": "5",
+          "numDivLineDashed": "0",
+          "numVDivLines": "5",
+          "vDivLineColor": "lightgrey",
+          "vDivLineThickness": "1",
+          "vDivLineAlpha": "70",
+          "vDivLineDashed": "0",
+          "vDivLineDashLen": "5",
+          "vDivLineDashGap": "3",
+          "showAlternateVGridColor": "1" 
+
       },
       "data": [
           {
@@ -70,7 +88,9 @@ export class HomeComponent implements OnInit {
   }
 
   onRestaurantOpenCloseChange () {
-
+    // if(this.restaurantOpenClose == true){
+    //   this.fontColor= {'color': 'green'};
+    // }
   }
 
 }
