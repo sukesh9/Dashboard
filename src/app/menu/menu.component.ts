a8880ffa4db14aea339d3340375927a8c31b8aff
unknown <sukeshpsk2@gmail.com>
2018-08-03 00:23:51 -0500

    menu add item created

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { DashboardService } from '../dashboard.service'
+import { DashboardService } from '../dashboard.service';
+
+import { MenuItem } from '../menu/menu.model'
  
 @Component({
   selector: 'app-menu',
@@ -11,17 +13,23 @@ export class MenuComponent implements OnInit {
 
   constructor(private dashboardService: DashboardService) { }
 
-  menuData: any;
+  menuData: Array<MenuItem>;        // Json to populate the view
+  selectedMenuItemToEdit: MenuItem;  // used to open the editing menu item in modal
 
-  selectedMenuItemToEdit: any;
+  menuType: string;                  // Change view according to the selected menu type from dropdown
+  menuTypes: Array<string>;         // Dropdown variable
 
-  menuType: string;
-  menuTypes: Array<string>;
+  selectedCategory: string;         // scroll to the selected category from drop down
+
+  addItems: Array<string>;           // Drop down list to add 
+  addMenuItem: MenuItem;
 
   ngOnInit() {
     this.menuTypes = ["Catering Menu", "Full Menu", "Breakfast Menu", "Lunch Menu"];
+    this.addItems = ["Add Category", "Add Item"];
     this.menuType = this.menuTypes[0];
     this.getMenu(this.menuType);
+    this.addMenuItem = new MenuItem();
 
   }
 
@@ -41,7 +49,12 @@ export class MenuComponent implements OnInit {
   }
 
   onMenuTypeChange(type: string){
+    this.menuType = type;
     this.getMenu(type);
   }
+
+  scroll(element){   
+    var ele = document.getElementById(element);   
+    window.scrollTo(ele.offsetLeft,ele.offsetTop); }
 
 }
---
+++ src/app/menu/menu.model.ts
@@ -0,0 +1,9 @@
+export class MenuItem{
+    id: string;
+    title: string;
+    category: string;
+    description: string;
+    availability: string;
+    price: string;
+
+}
